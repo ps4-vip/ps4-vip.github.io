9915503cefcece32bf265eebbbc22f480e845616
ps4-vip <z00m.fx50@gmail.com>
2026-08-18 01:34:50 +0300

    Update cache version for restored script

--- includes/script.js
+++ includes/script.js
@@ -159,7 +159,7 @@ function displayCacheProgress() {
 // ========================================================
 
 document.addEventListener("DOMContentLoaded", function () {
-  // AppCache
+    // AppCache
 
   if (window.applicationCache) {
     window.applicationCache.addEventListener("progress", cacheProgress, false);
@@ -169,7 +169,25 @@ document.addEventListener("DOMContentLoaded", function () {
     window.applicationCache.onupdateready = displayCacheProgress;
   }
 
-  // Selected exploit
+  // Offline Auto Jailbreak
+
+if (!navigator.onLine && window.applicationCache) {
+
+  if (
+    window.applicationCache.status === window.applicationCache.CACHED ||
+    window.applicationCache.status === window.applicationCache.UPDATEREADY
+  ) {
+
+    if (window.zeekoShowLoading) {
+      window.zeekoShowLoading();
+    }
+
+    doJb();
+  }
+
+}
+
+// Selected exploit
 
   if (exploitChain === "netctrl") {
     netctrlRadio.checked = true;
