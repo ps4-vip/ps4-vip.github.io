d9d77dc342d2b6ec439f7dd4ab2bccb8e44d4a7f
ps4-vip <z00m.fx50@gmail.com>
2026-08-30 16:30:16 +0300

    Restore loading message

--- includes/script.js
+++ includes/script.js
@@ -66,7 +66,7 @@ function displayCacheProgress() {
   const status = document.getElementById("status");
 
   if (status) {
-    status.textContent = "GoldHEN v2.4b18.10 Loaded Successfully ✓";
+    status.textContent = "Loading GoldHEN v2.4b18.10... Please Wait";
   }
 
   setTimeout(function () {
