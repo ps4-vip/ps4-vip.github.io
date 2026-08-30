bded1c3c432620de601d4a114e054def813b5e98
ps4-vip <z00m.fx50@gmail.com>
2026-08-30 19:23:50 +0300

    Fix online loading message flow

--- includes/script.js
+++ includes/script.js
@@ -56,16 +56,14 @@ function startAutoJbAfterCache() {
 
   setTimeout(function () {
       const traceStatus = document.getElementById("status");
-      if (traceStatus) traceStatus.textContent = "TRACE: AUTO-JB STARTED";
       if (!navigator.onLine) doJb();
-      if (traceStatus) traceStatus.textContent = "TRACE: AUTO-JB CALLED";
   }, 2000);
 }
 
 function displayCacheProgress() {
   const status = document.getElementById("status");
 
-  if (status) {
+  if (status && !navigator.onLine) {
     status.textContent = "Loading GoldHEN v2.4b18.10... Please Wait";
   }
 
@@ -107,10 +105,6 @@ document.addEventListener("DOMContentLoaded", function () {
           if (appCache.status !== lastAppCacheStatus) {
             lastAppCacheStatus = appCache.status;
 
-            if (traceStatus) {
-              traceStatus.textContent =
-                "TRACE: APPCACHE STATUS CHANGED = " + lastAppCacheStatus;
-            }
 
             if (
               appCache.status === appCache.IDLE ||
@@ -120,9 +114,6 @@ document.addEventListener("DOMContentLoaded", function () {
             }
           }
         }, 1000);
-        if (traceStatus) {
-          traceStatus.textContent = "TRACE: APPCACHE FOUND / STATUS = " + appCache.status;
-        }
       appCache.addEventListener("progress", cacheProgress, false);
 
       appCache.oncached = displayCacheProgress;
